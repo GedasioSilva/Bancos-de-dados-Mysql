50b6b90f809986792ec99cc1cc1b911b86156e41
Gedasio Silva <81691619+GedasioSilva@users.noreply.github.com>
2021-08-04 19:56:05 -0300

    Aula 04/08/2021
    Exercitando

--- BoSon Treinamentos/Banco de dados DB_Biblioteca/DB_biblioteca.sql
+++ BoSon Treinamentos/Banco de dados DB_Biblioteca/DB_biblioteca.sql
@@ -185,13 +185,111 @@ select Sum(preco_livro) from tbl_livro;
  set  nome = "Gedasiio" where codigo = 1;
  -- comando pra alter os registros atualizar o nome na tabela teste incremento;
  
+ select * from tbl_livro
+ where Data_Pub between '20111213' and '20201121';
+ -- comando Between pra consultar entre os intervalos 
  
+ select nome_livro as livro,preco_livro as preço from tbl_livro
+ where id_autores between 55.33 and 70.66;
  
-  
-  
-  
-  
-  
+ select * from tbl_livro;
+ 
+ select  sobreNome_autor as Last_nome from tbl_autores
+ where ID_Autores between  1 and 3 ;
+ 
+select Nome_Livro as Nome,Preco_Livro as Preço from tbl_livro
+where Nome_Livro like 'J%';
+-- comando like pista nome do livro que começe com a letra J
+
+select nome_livro as Nome,preco_livro as preço from tbl_livro
+where Nome_Livro not like 'J%';
+-- Comando Not Like pesquisa nome do livro que não começe com A Letra J;
+
+select nome_livro as nome , preco_livro as Preço from tbl_livro
+where nome_livro like '_A%';
+-- livro cujo a segunda letra do nome seja A;
+
+select * from tbl_livro;
+
+select nome_livro from tbl_livro 
+where Nome_Livro regexp '^[PC]';
+-- Consulta os nomes do livros que começam com P OU C 
+
+select nome_livro from tbl_livro
+where nome_livro regexp '^[^JH]';
+-- Buscar nomes de livros que não começem com J OU H 
+
+select nome_livro from tbl_livro
+Where nome_livro regexp '[pa]$';
+-- Nomes de Livros que terminem com P ou A 
+
+select nome_livro from tbl_livro
+Where nome_livro regexp '^[JH]|PHP';
+-- pesquisar os nomes de livros que começem com J OU H ou PHp
+
+select * from tbl_autores;
+
+alter table tbl_autores
+modify column SobreNome_autor varchar(60)
+default 'Da Silva';
+-- colocando valor padrão no sobre nome autor caso eu não coloque nada
+
+ select * from vendas;
+ 
+ update tbl_autores
+ set sexo = "F" where id_autores = 5;
+
+create table if not exists vendas(
+id smallint primary key,
+nome_vendedor varchar(20),
+quantidade int,
+produto varchar(20),
+cidade varchar(20)
+) default charset = "utf8";
+
+insert into vendas(id,nome_vendedor,quantidade,produto,cidade) 
+values (19,"Roberto",3145,"Mouse","São Paulo");
+
+select * from vendas;
+
+select id_editora,count(*)
+from tbl_livro 
+group by Id_Editora;
+
+select cidade ,sum(quantidade) as total
+from vendas
+group by cidade;
+
+select cidade,count(*) as Total
+from vendas
+group by cidade;
+
+-- Group by é usado pra fazer consultas em grupo;
+
+select cidade,sum(quantidade) 
+from vendas
+group by cidade
+having sum(quantidade) < 2500;
+
+select cidade,sum(quantidade) as total
+from vendas
+where produto = 'WebCam'  
+group by cidade;
+
+create view vw_LivroAutores
+as select tbl_livro.nome_livro as Livro,
+tbl_autores.Nome_Autor as Autor
+from tbl_livro
+inner join tbl_autores
+on tbl_livro.Id_autor = tbl_autores.Id_autores;
+-- Criando Tabela views consulta tbls livro e autores  
+
+select livro,autor
+from vw_livroautores
+order by autor;
+-- Consultando o Views
+
+aula 33
 
 
-
+ 
